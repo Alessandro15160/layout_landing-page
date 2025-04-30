343d096573536b6d9b099ff730c40b8be1eb7957
Alessandro <Alessandrogabriel15160@gmail.com>
2025-04-30 16:22:28 -0300

    make build

---
+++ index.35b5cb95.js
@@ -0,0 +1,2 @@
+document.querySelector(".main__contacts__form").addEventListener("submit",function(t){t.preventDefault();var e=document.querySelector(".main__contacts__form__button");e.textContent="Form submitted",e.style.backgroundColor="green",this.reset()});
+//# sourceMappingURL=index.35b5cb95.js.map
